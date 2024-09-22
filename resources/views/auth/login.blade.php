536699be7a0986f6eb977568a6c8839b034e2b39
Atif Usman <atifusmanhashim@gmail.com>
2024-09-23 02:07:04 +0500

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,10 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login Page</title>
+    <title>Know Your Employee - Login Page</title>
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="icon" href="{{ asset('knowyouremployee.ico') }}" type="image/x-icon"/>
     <style>
         body, html {
             height: 100%;
